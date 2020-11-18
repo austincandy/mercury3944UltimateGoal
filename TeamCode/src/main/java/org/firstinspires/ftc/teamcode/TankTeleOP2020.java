c33eef4ccad6e1fa6db194e8a68e52b98a0e73b4
AustinK <austin.koshy.mail@gmail.com>
2020-11-18 13:18:34 -0500

    Made a test for the new chassis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTeleOP2020.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTeleOP2020.java
@@ -7,18 +7,13 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp
 public class TankTeleOP2020 extends LinearOpMode {
 
-    private DcMotor frontRight;
-    private DcMotor frontLeft;
-    private DcMotor backRight;
-    private DcMotor backLeft;
-
+    private DcMotor rightFront;
+    private DcMotor leftFront;
 
     @Override
     public void runOpMode() {
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -30,16 +25,14 @@ public class TankTeleOP2020 extends LinearOpMode {
         while (opModeIsActive()) {
             telemetry.addData("Status", "Running");
             telemetry.update();
-            leftPower = -this.gamepad1.left_stick_y;
-            rightPower = -this.gamepad1.right_stick_y;
+            leftPower = (this.gamepad1.left_stick_y)/2;
+            rightPower = (-this.gamepad1.right_stick_y)/2;
 
-            frontRight.setPower(rightPower);
-            frontLeft.setPower(leftPower);
-            backRight.setPower(rightPower);
-            backLeft.setPower(leftPower);
+            rightFront.setPower(rightPower);
+            leftFront.setPower(leftPower);
 
-            telemetry.addData("Right Power", frontRight.getPower());
-            telemetry.addData("Left Power", frontLeft.getPower());
+            telemetry.addData("Right Power", rightFront.getPower());
+            telemetry.addData("Left Power", leftFront.getPower());
         }
     }
 }
